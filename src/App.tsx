a9bf6dffb208a513ef47cdb97de79762c27391b7
kaaalin <47788940+kaaalin@users.noreply.github.com>
2025-12-04 23:10:55 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1376,7 +1376,7 @@ export default function App() {
     
 
       {/* LINE 2 — GAME MESSAGE (Illegal move, Check, Winner, etc.) */}
-      {gs.message}<div className="text-[11px] sm:text-xs"> -> Turn: <b><span className="font-bold capitalize"> {gs.turn}</span></b>
+      {gs.message}<div className="text-[11px] sm:text-xs"> → Turn: <b><span className="font-bold capitalize"> {gs.turn}</span></b>
         
       </div>
     </button>
