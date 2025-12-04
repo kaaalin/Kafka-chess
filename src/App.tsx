89f3177ab544ac67e5943e3e962e1000a3d8bf95
kaaalin <47788940+kaaalin@users.noreply.github.com>
2025-12-04 15:56:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1240,10 +1240,10 @@ export default function App() {
          
 <a
   href="https://www.buymeacoffee.com/kalinyanev"
-  className="inline-block border-[0.5px] border-white rounded-lg p-[-5x]"
+  className="inline-block border-[0.5px] border-white rounded-lg p-[5x]"
 >
   <img
-    src="https://img.buymeacoffee.com/button-api/?text=Buy%20the%20authors%20a%20coffee&emoji=☕&slug=kalinyanev&button_colour=171717&font_colour=ffffff&font_family=Poppins&outline_colour=ffffff&coffee_colour=83b2be"
+    src="https://img.buymeacoffee.com/button-api/?text=Buy%20the%20authors%20a%20coffee&emoji=☕&slug=kalinyanev&button_colour=171717&font_colour=ffffff&font_family=Inter&outline_colour=ffffff&coffee_colour=83b2be"
     className="block mx-auto"
   />
 </a>
