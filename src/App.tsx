4b94f3fd9028201cd48ca3ea7dec3bca5ba2672a
kaaalin <47788940+kaaalin@users.noreply.github.com>
2025-12-05 14:19:08 +0200

    hard-default

--- src/App.tsx
+++ src/App.tsx
@@ -200,7 +200,7 @@ function initialGame(): GameState {
     message: null,
     winner: null,
     winReason: null,
-    ai: { mode: "human", cpuPlays: "black", level: "Medium" },
+    ai: { mode: "human", cpuPlays: "black", level: "Hard" },
     lastMove: null,
     repetition: {},
   };
