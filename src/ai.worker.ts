3bd0a2ec3bde3947781eb73e3d0717e7cb636751
kaaalin <47788940+kaaalin@users.noreply.github.com>
2025-12-30 15:59:35 +0200

    Update ai.worker.ts

--- src/ai.worker.ts
+++ src/ai.worker.ts
@@ -1,6 +1,6 @@
 /// <reference lib="webworker".. />
 
-import { pickAiMove, type GameState } from "./engine";
+import { pickAiMove, type GameState } from "./engine.ts";
 
 self.onmessage = (e: MessageEvent) => {
   const { id, gs } = e.data as { id: number; gs: GameState };
