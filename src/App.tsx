e36f61ba39fd46ac0bdf4ddb111248131a402c7a
kaaalin <47788940+kaaalin@users.noreply.github.com>
2026-01-03 23:30:04 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1253,7 +1253,7 @@ const BlueSymbol = ({ type, flip }: { type: PieceType; flip?: boolean }) => (
   textAnchor="middle"
   fontSize="84"
   fill="none"
-  stroke="rgba(245,245,245,0.85)"   // light overlay
+  stroke="rgba(0,0,0,95)"   // light overlay
   strokeWidth="6"
   fontFamily="'Noto Chess','DejaVu Sans',serif"
 >
